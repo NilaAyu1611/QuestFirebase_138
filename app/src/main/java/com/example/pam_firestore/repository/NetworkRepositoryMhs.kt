0ec65d59303987879dfba75a91adf4f9c7c9c84e
NilaAyu1611 <nilaayucw@gmail.com>
2025-01-12 10:06:00 +0700

    Fungsi getMhs untuk mendapatkan data mhs berdasarkan nim scr real-time.

--- app/src/main/java/com/example/pam_firestore/repository/NetworkRepositoryMhs.kt
+++ app/src/main/java/com/example/pam_firestore/repository/NetworkRepositoryMhs.kt
@@ -3,6 +3,7 @@ package com.example.pam_firestore.repository
 import com.example.pam_firestore.model.Mahasiswa
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.toObject
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
@@ -25,7 +26,7 @@ class NetworkRepositoryMhs(
 
     override fun getAllMahasiswa(): Flow<List<Mahasiswa>> = callbackFlow{      //dengan riltime tanpa melakukan event
         val mhsCollection = firestore.collection("Mahasiswa")
-            .orderBy("nim",Query.Direction.ASCENDING)
+            .orderBy("nim",Query.Direction.DESCENDING)
             .addSnapshotListener{
                 value, error ->
                 if(value != null){
@@ -34,7 +35,7 @@ class NetworkRepositoryMhs(
                         it.toObject(Mahasiswa::class.java)
                     }
                     //fungsi untuk mengirim collection ke db
-                    trySend(mhsList)
+                    trySend(mhsList) // try send memberikan fungsi untuk mengirim data ke flow
                 }
             }
         awaitClose{
@@ -43,8 +44,19 @@ class NetworkRepositoryMhs(
         }
     }
 
-    override fun getMhs(nim: String): Flow<Mahasiswa> {
-        TODO("Not yet implemented")
+    override fun getMhs(nim: String): Flow<Mahasiswa> = callbackFlow{
+        // Mengakses dokumen mahasiswa berdasarkan NIM dari koleksi "Mahasiswa"
+        val mhsDocument = firestore.collection("Mahasiswa")
+            .document(nim)
+            .addSnapshotListener { value, error ->
+                if (value != null){
+                    val mhs = value.toObject(Mahasiswa::class.java) !!
+                }
+            }
+        // mentup listener saat flow tidak lagi digunakan
+        awaitClose{
+            mhsDocument.remove()        // menghapus listener untuk dokumen mhs
+        }
     }
 
     override suspend fun deleteMhs(mahasiswa: Mahasiswa) {
