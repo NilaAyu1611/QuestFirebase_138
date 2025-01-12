10f6e3f35fd2d327a1311c22be2ba33e1b8ce703
NilaAyu1611 <nilaayucw@gmail.com>
2025-01-12 13:28:34 +0700

    Memberikan umpan balik kepada user berdasarkan status operasi yang dilakukan

--- app/src/main/java/com/example/pam_firestore/ui/pages/InsertView.kt
+++ app/src/main/java/com/example/pam_firestore/ui/pages/InsertView.kt
@@ -2,12 +2,16 @@ package com.example.pam_firestore.ui.pages
 
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pam_firestore.ui.viewmodel.FormState
 import com.example.pam_firestore.ui.viewmodel.InsertViewModel
 import com.example.pam_firestore.ui.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
 fun InsertMhsView(
@@ -21,6 +25,29 @@ fun InsertMhsView(
     val snackbarHostState = remember { SnackbarHostState() }
     val coroutineScope = rememberCoroutineScope()
 
+    // Observasi perubahan state untuk snackbar dan navigasi
+    LaunchedEffect(uiState) {
+        when(uiState) {
+            is FormState.Success -> {
+                println(
+                    "InsertMhsView: uiState is FromState.Success, navigate to home" + uiState.message
+                )
+                coroutineScope.launch {
+                    snackbarHostState.showSnackbar(uiState.message) // Tampilkan snackbar
+                }
+                delay(700)
+                onNavigate() // Navigasi langsung
 
+                viewModel.resetSnackBarMessage()    // reset snackbar state
+            }
+
+            is FormState.Error -> {
+                coroutineScope.launch {
+                    snackbarHostState.showSnackbar(uiState.message)
+                }
+            }
+            else -> Unit
+        }
+    }
 
 }
