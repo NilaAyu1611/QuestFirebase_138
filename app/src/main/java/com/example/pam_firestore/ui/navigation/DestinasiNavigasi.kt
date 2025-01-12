7e746ff9ebe315001aa8d49adc095925996c02c5
NilaAyu1611 <nilaayucw@gmail.com>
2025-01-12 14:34:09 +0700

    Membuat object DestinasiHome untuk mengatur rute hal.home

--- app/src/main/java/com/example/pam_firestore/ui/navigation/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/pam_firestore/ui/navigation/DestinasiNavigasi.kt
@@ -4,3 +4,8 @@ interface DestinasiNavigasi {       // untuk menyimpan riuting halaman
     val route: String
     val titleRes: String
 }
+
+object DestinasiHome : DestinasiNavigasi{
+    override val route: String = "home"
+    override val titleRes: String = "Home"
+}
